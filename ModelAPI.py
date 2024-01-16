302617e5863752ff530910c2eaf8a44f2bb596b2
Cristian Maldonado <CAMaldonadobarreto@student.fullsail.edu>
2024-01-15 23:45:37 -0500

    Modified script

--- ModelAPI.py
+++ ModelAPI.py
@@ -6,12 +6,9 @@ from io import BytesIO
 import urllib.request 
 from urllib.request import urlopen
 
-# API Endpoints
-textToImage = "https://stablediffusionapi.com/api/v3/text2img"
-ImageToImage = "https://stablediffusionapi.com/api/v3/img2img"
-InPainting = "https://stablediffusionapi.com/api/v3/inpaint"
+url = "https://stablediffusionapi.com/api/v3/text2img"
+url2 = "https://stablediffusionapi.com/api/v3/img2img"
 
-# Payload for Json bosy request
 payload = json.dumps({
     "key": "",
     "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner))",
@@ -36,30 +33,24 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-# Calling the API itself
-response = requests.request("POST", textToImage, headers=headers, data=payload)
+response = requests.request("POST", url, headers=headers, data=payload)
 
-# Loading object into a List
 obj = json.loads(response.text)
 
-# Storing Output value from List
 output = obj['output']
 
-# Convert list to string
 strout = str(output)
 
-# Truncating String 
 str = strout[2:]
 str2 = str[:-2]
-outPut_url = str2
+url = str2
 
-# Downloading Image
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-Agent', 'MyApp/1.0')]
 urllib.request.install_opener(opener)
 
 
-urllib.request.urlretrieve(outPut_url, "pornstar.png") 
+urllib.request.urlretrieve(url, "pornstar.png") 
 
 img = Image.open(r"pornstar.png") 
 img.show()
