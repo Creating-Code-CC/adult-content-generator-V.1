6fc44e8e512d863ab2837d1c5c8bcfd14a98d3f7
Cristian Maldonado <CAMaldonadobarreto@student.fullsail.edu>
2024-01-15 23:35:43 -0500

    Added comment clarifications

--- ModelAPI.py
+++ ModelAPI.py
@@ -6,10 +6,12 @@ from io import BytesIO
 import urllib.request 
 from urllib.request import urlopen
 
+# API Endpoints
 textToImage = "https://stablediffusionapi.com/api/v3/text2img"
 ImageToImage = "https://stablediffusionapi.com/api/v3/img2img"
 InPainting = "https://stablediffusionapi.com/api/v3/inpaint"
 
+# Payload for Json bosy request
 payload = json.dumps({
     "key": "",
     "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner))",
@@ -34,18 +36,24 @@ headers = {
     'Content-Type': 'application/json',
 }
 
+# Calling the API itself
 response = requests.request("POST", textToImage, headers=headers, data=payload)
 
+# Loading object into a List
 obj = json.loads(response.text)
 
+# Storing Output value from List
 output = obj['output']
 
+# Convert list to string
 strout = str(output)
 
+# Truncating String 
 str = strout[2:]
 str2 = str[:-2]
 outPut_url = str2
 
+# Downloading Image
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-Agent', 'MyApp/1.0')]
 urllib.request.install_opener(opener)
